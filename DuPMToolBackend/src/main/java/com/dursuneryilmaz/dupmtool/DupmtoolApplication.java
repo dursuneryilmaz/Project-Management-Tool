402675d06dc457796bb303e81b2c3ff1f6487eb9
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-17 19:05:50 +0300

    initial project commit

---
+++ DuPMToolBackend/src/main/java/com/dursuneryilmaz/dupmtool/DupmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.dursuneryilmaz.dupmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DupmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DupmtoolApplication.class, args);
+    }
+
+}
---
+++ DuPMToolBackend/src/test/java/com/dursuneryilmaz/dupmtool/DupmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dursuneryilmaz.dupmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DupmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
