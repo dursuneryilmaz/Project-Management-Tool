0b4727a67b5ad25ade840bc38f183d5e4967d04d
Jhon Baron <jhon.baron@zabud.com.co>
2020-12-18 20:10:08 -0500

    updated: removed not used imports and use @RestControllerAdvice

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/exception/AppResponseEntityExceptionHandler.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/exception/AppResponseEntityExceptionHandler.java
@@ -4,14 +4,12 @@ import com.dursuneryilmaz.duscrumtool.model.response.ProjectCodeExceptionModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-@RestController
+@RestControllerAdvice
 public class AppResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {ProjectCodeException.class})
     public ResponseEntity<Object> handleUserServiceException(ProjectCodeException ex, WebRequest request) {
