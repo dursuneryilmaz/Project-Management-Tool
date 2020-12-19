5fe61cfaa0024451bf9d4735b0fd09e1a2261384
Jhon Baron <jhon.baron@zabud.com.co>
2020-12-18 20:11:03 -0500

    updated: added serial id version by serializable way

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/exception/ProjectCodeException.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/exception/ProjectCodeException.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProjectCodeException extends RuntimeException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 5874968188971296689L;
+
     public ProjectCodeException(String message) {
         super(message);
     }
