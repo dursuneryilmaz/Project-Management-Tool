2037ca68550a002d07d822e7532bb3e5cece6a00
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-20 15:42:12 +0300

    Update ProductService.java

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/service/ProductService.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/service/ProductService.java
@@ -2,12 +2,14 @@ package com.dursuneryilmaz.duscrumtool.service;
 
 import com.dursuneryilmaz.duscrumtool.domain.Product;
 
+import java.util.List;
+
 public interface ProductService {
     Product createProduct(Product product);
 
-    Product findProductById(String productId);
+    Product getProductById(String productId);
 
-    Iterable<Product> findAllProducts();
+    List<Product> getAllProducts();
 
     Product updateProductById(String productId, Product product);
 
