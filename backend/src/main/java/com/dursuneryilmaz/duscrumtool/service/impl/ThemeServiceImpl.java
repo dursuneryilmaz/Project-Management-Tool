291c69bf3f579673ad8ed20ad16d540a068f179b
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-20 15:44:52 +0300

    createTheme with product
    getAllByProduct not return empty list anymore

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/service/impl/ThemeServiceImpl.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/service/impl/ThemeServiceImpl.java
@@ -2,10 +2,12 @@ package com.dursuneryilmaz.duscrumtool.service.impl;
 
 import com.dursuneryilmaz.duscrumtool.domain.Product;
 import com.dursuneryilmaz.duscrumtool.domain.Theme;
-import com.dursuneryilmaz.duscrumtool.exception.ProjectCodeException;
+import com.dursuneryilmaz.duscrumtool.exception.ProductIdException;
 import com.dursuneryilmaz.duscrumtool.model.response.ExceptionMessages;
+import com.dursuneryilmaz.duscrumtool.repository.ProductRepository;
 import com.dursuneryilmaz.duscrumtool.repository.ThemeRepository;
 import com.dursuneryilmaz.duscrumtool.service.ThemeService;
+import com.dursuneryilmaz.duscrumtool.shared.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,18 +17,28 @@ import java.util.List;
 public class ThemeServiceImpl implements ThemeService {
     @Autowired
     ThemeRepository themeRepository;
+    @Autowired
+    Utils utils;
 
     @Override
-    public List<Theme> findAllByProduct(Product product) {
+    public Theme createTheme(Theme theme, Product product) {
+        theme.setThemeId(utils.generatePublicId(32));
+        theme.setProduct(product);
+        return themeRepository.save(theme);
+    }
+
+    @Override
+    public List<Theme> getAllByProduct(Product product) {
         List<Theme> themeList = themeRepository.findAllByProduct(product);
-        if (themeList == null) throw new ProjectCodeException(ExceptionMessages.NO_RECORDS_FOUND.getExceptionMessage());
+        if (themeList.size() == 0)
+            throw new ProductIdException(ExceptionMessages.NO_RECORDS_FOUND.getExceptionMessage());
         return themeList;
     }
 
     @Override
-    public Theme findByThemeId(String themeId) {
+    public Theme getByThemeId(String themeId) {
         Theme theme = themeRepository.findByThemeId(themeId);
-        if (theme == null) throw new ProjectCodeException(ExceptionMessages.NO_RECORD_FOUND.getExceptionMessage());
+        if (theme == null) throw new ProductIdException(ExceptionMessages.NO_RECORD_FOUND.getExceptionMessage());
         return theme;
     }
 }
