2078ed9523267c5791b6a20d30330834d69fe45e
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-20 15:46:06 +0300

    crud to jpa repository

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/repository/ProductRepository.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/repository/ProductRepository.java
@@ -1,13 +1,10 @@
 package com.dursuneryilmaz.duscrumtool.repository;
 
 import com.dursuneryilmaz.duscrumtool.domain.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByProductId(String publicId);
-
-    @Override
-    Iterable<Product> findAll();
 }
--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/repository/ThemeRepository.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/repository/ThemeRepository.java
@@ -2,13 +2,13 @@ package com.dursuneryilmaz.duscrumtool.repository;
 
 import com.dursuneryilmaz.duscrumtool.domain.Product;
 import com.dursuneryilmaz.duscrumtool.domain.Theme;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ThemeRepository extends CrudRepository<Theme, Long> {
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
     List<Theme> findAllByProduct(Product product);
 
     Theme findByThemeId(String themeId);
