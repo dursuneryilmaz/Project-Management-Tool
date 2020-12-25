eabfe616ce9e4ef103ff7362e4cda4f0a3ae72cb
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-12-26 01:52:13 +0300

    created: empty task controller

---
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/controller/TaskController.java
@@ -0,0 +1,11 @@
+package com.dursuneryilmaz.duscrumtool.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+public class TaskController {
+
+    // lots of endpoints
+}
