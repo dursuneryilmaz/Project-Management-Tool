066b645a1c4b3c3aa493d591d13627e665f5e1c5
Dursun ERYILMAZ <dursun1147@gmail.com>
2021-01-03 20:48:39 +0300

    update: only login and register endpoint opened publicly
    method overridings

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/security/WebSecurityConfig.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/security/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package com.dursuneryilmaz.duscrumtool.security;
 
 import com.dursuneryilmaz.duscrumtool.security.jwt.JwtAuthenticationEntryPoint;
+import com.dursuneryilmaz.duscrumtool.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +25,24 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 )
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
+    UserService userService;
+    @Autowired
     JwtAuthenticationEntryPoint unAuthenticatedHandler;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -40,7 +64,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.html",
                         "/**/*.css",
                         "/**/*.js"
-                // create URIs which does not required authentication in a class named SecurityConstant
-                ).permitAll();//.anyRequest().authenticated();
+                ).permitAll()
+                .antMatchers(SecurityConstants.REGISTER_URI).permitAll()
+                .antMatchers(SecurityConstants.LOGIN_URI).permitAll()
+                .antMatchers(SecurityConstants.H2_CONSOLE_URI).permitAll()
+                .anyRequest().authenticated();
+
     }
 }
