ebaeb0fa4eca15d4646e044d72a84c2a40cb78bd
Dursun ERYILMAZ <dursun1147@gmail.com>
2021-01-03 12:15:23 +0300

    update: password encoder bean added

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/DuScrumToolApplication.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/DuScrumToolApplication.java
@@ -2,12 +2,18 @@ package com.dursuneryilmaz.duscrumtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DuscrumtoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DuscrumtoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DuscrumtoolApplication.class, args);
+    }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
