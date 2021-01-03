867a518db950ea83a2d695dfd00e5cd62e8b8f93
Dursun ERYILMAZ <dursun1147@gmail.com>
2021-01-03 20:46:08 +0300

    create: user response model created

---
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/model/response/UserResponseModel.java
@@ -0,0 +1,44 @@
+package com.dursuneryilmaz.duscrumtool.model.response;
+
+import java.io.Serializable;
+
+public class UserResponseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
