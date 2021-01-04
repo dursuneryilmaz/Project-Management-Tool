10ccdae842eadbd05cff5e0cf327005b2000f980
Dursun ERYILMAZ <dursun1147@gmail.com>
2021-01-04 16:41:07 +0300

    create: user reset password request model
    this model used for requesting password

---
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/model/request/UserResetPasswordRequestModel.java
@@ -0,0 +1,13 @@
+package com.dursuneryilmaz.duscrumtool.model.request;
+
+public class UserResetPasswordRequestModel {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
