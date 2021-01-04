3ff8ddfd46db2d541f665427a79220111d451b7d
Dursun ERYILMAZ <dursun1147@gmail.com>
2021-01-04 16:34:36 +0300

    update: added new constants
    auth token exp time and token secret
    pass reset token exp time and token secret
    email verify token exp time and token secret
    fix uri for h2 console

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/security/SecurityConstants.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/security/SecurityConstants.java
@@ -4,17 +4,22 @@ public class SecurityConstants {
     // these can be fetched from application properties or yml
 
     // times
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long AUTH_TOKEN_EXPIRATION_TIME = 864_000_000; // 10 days
     public static final long PASSWORD_RESET_TOKEN_EXPIRATION_TIME = 3_600_000; // 1hour
+    public static final long EMAIL_VERIFICATION_TOKEN_EXPIRATION_TIME = 3_600_000; // 1hour
+
     // token properties
     public static final String HEADER_STRING = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String TOKEN_SECRET = "tokenSecret";
+    public static final String AUTH_TOKEN_SECRET = "authTokenSecret";
+    public static final String EMAIL_VERIFICATION_TOKEN_SECRET = "emailVerificationTokenSecret";
+    public static final String PASSWORD_RESET_TOKEN_SECRET = "passwordResetTokenSecret";
+
     // URIs
     public static final String REGISTER_URI = "/api/v1/users/register";
     public static final String LOGIN_URI = "/api/v1/users/login";
     public static final String EMAIL_VERIFICATION_URI = "/api/v1/users/email-verification";
     public static final String PASSWORD_RESET_REQUEST_URI = "/api/v1/users/password-reset-request";
     public static final String PASSWORD_RESET_URI = "/api/v1//users/password-reset";
-    public static final String H2_CONSOLE_URI = "h2-console/**";
+    public static final String H2_CONSOLE_URI = "/h2-console/**";
 }
