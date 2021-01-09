599864b34a740e56d56bf492adaec087ac4f35e8
Dursun ERYILMAZ <dursun1147@gmail.com>
2021-01-09 14:11:52 +0300

    update: logged in user based request matching
    these operations must be in service layer ?

--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/controller/EpicController.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/controller/EpicController.java
@@ -3,10 +3,8 @@ package com.dursuneryilmaz.duscrumtool.controller;
 import com.dursuneryilmaz.duscrumtool.domain.Epic;
 import com.dursuneryilmaz.duscrumtool.domain.Story;
 import com.dursuneryilmaz.duscrumtool.domain.Theme;
-import com.dursuneryilmaz.duscrumtool.service.EpicService;
-import com.dursuneryilmaz.duscrumtool.service.RequestValidationService;
-import com.dursuneryilmaz.duscrumtool.service.StoryService;
-import com.dursuneryilmaz.duscrumtool.service.ThemeService;
+import com.dursuneryilmaz.duscrumtool.domain.User;
+import com.dursuneryilmaz.duscrumtool.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -26,21 +25,34 @@ public class EpicController {
     @Autowired
     StoryService storyService;
     @Autowired
+    UserService userService;
+    @Autowired
     RequestValidationService requestValidationService;
+
 
     // create epic
     @PostMapping(path = "/{themeId}")
-    public ResponseEntity<?> createEpic(@PathVariable String themeId, @Valid @RequestBody Epic epic, BindingResult bindingResult) {
+    public ResponseEntity<?> createEpic(@PathVariable String themeId, @Valid @RequestBody Epic epic,
+                                        BindingResult bindingResult, Principal principal) {
         ResponseEntity<?> errorMap = requestValidationService.mapValidationErrors(bindingResult);
         if (errorMap != null) return errorMap;
         Theme theme = themeService.getThemeById(themeId);
-        return new ResponseEntity<Epic>(epicService.createEpic(epic, theme), HttpStatus.CREATED);
+        User user = userService.getUserByEmail(principal.getName());
+        boolean contains = theme.getProduct().getScrumManagerList().contains(user);
+        if (contains) return new ResponseEntity<Epic>(epicService.createEpic(epic, theme), HttpStatus.CREATED);
+
+        return requestValidationService.getAccessDeniedResponseEntity();
     }
 
     // get epic's stories
     @GetMapping(path = "/{epicId}/stories")
-    public ResponseEntity<List<Story>> getEpicStories(@PathVariable String epicId) {
+    public ResponseEntity<?> getEpicStories(@PathVariable String epicId, Principal principal) {
         Epic epic = epicService.getEpicById(epicId);
-        return new ResponseEntity<List<Story>>(storyService.getAllByEpic(epic), HttpStatus.OK);
+        User user = userService.getUserByEmail(principal.getName());
+        boolean managerContains = epic.getTheme().getProduct().getScrumManagerList().contains(user);
+        boolean stakeHolderContains = epic.getTheme().getProduct().getStakeHolderList().contains(user);
+        if (managerContains | stakeHolderContains)
+            return new ResponseEntity<List<Story>>(storyService.getAllByEpic(epic), HttpStatus.OK);
+        return requestValidationService.getAccessDeniedResponseEntity();
     }
 }
--- backend/src/main/java/com/dursuneryilmaz/duscrumtool/controller/StoryController.java
+++ backend/src/main/java/com/dursuneryilmaz/duscrumtool/controller/StoryController.java
@@ -3,10 +3,9 @@ package com.dursuneryilmaz.duscrumtool.controller;
 import com.dursuneryilmaz.duscrumtool.domain.Epic;
 import com.dursuneryilmaz.duscrumtool.domain.Story;
 import com.dursuneryilmaz.duscrumtool.domain.Task;
-import com.dursuneryilmaz.duscrumtool.service.EpicService;
-import com.dursuneryilmaz.duscrumtool.service.RequestValidationService;
-import com.dursuneryilmaz.duscrumtool.service.StoryService;
-import com.dursuneryilmaz.duscrumtool.service.TaskService;
+import com.dursuneryilmaz.duscrumtool.domain.User;
+import com.dursuneryilmaz.duscrumtool.model.response.OperationModel;
+import com.dursuneryilmaz.duscrumtool.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -26,21 +26,35 @@ public class StoryController {
     @Autowired
     TaskService taskService;
     @Autowired
+    UserService userService;
+    @Autowired
     RequestValidationService requestValidationService;
+
 
     // create story
     @PostMapping(path = "/{epicId}")
-    public ResponseEntity<?> createStory(@PathVariable String epicId, @Valid @RequestBody Story story, BindingResult bindingResult) {
+    public ResponseEntity<?> createStory(@PathVariable String epicId, @Valid @RequestBody Story story, Principal principal,
+                                         BindingResult bindingResult) {
         ResponseEntity<?> errorMap = requestValidationService.mapValidationErrors(bindingResult);
         if (errorMap != null) return errorMap;
         Epic epic = epicService.getEpicById(epicId);
-        return new ResponseEntity<Story>(storyService.createStory(story, epic), HttpStatus.CREATED);
+        boolean contains = epic.getTheme().getProduct().getScrumManagerList().contains(
+                userService.getUserByEmail(principal.getName()));
+        if (contains) return new ResponseEntity<Story>(storyService.createStory(story, epic), HttpStatus.CREATED);
+        return requestValidationService.getAccessDeniedResponseEntity();
     }
 
     // get story's tasks
     @GetMapping(path = "/{storyId}/tasks")
-    public ResponseEntity<List<Task>> getEpicStories(@PathVariable String storyId) {
+    public ResponseEntity<?> getEpicStories(@PathVariable String storyId, Principal principal) {
         Story story = storyService.getStoryById(storyId);
-        return new ResponseEntity<List<Task>>(taskService.getAllByStory(story), HttpStatus.OK);
+        User user = userService.getUserByEmail(principal.getName());
+
+        boolean managerContains = story.getEpic().getTheme().getProduct().getScrumManagerList().contains(user);
+        boolean stakeHolderContains = story.getEpic().getTheme().getProduct().getStakeHolderList().contains(user);
+        boolean developerContains = story.getEpic().getTheme().getProduct().getProductDeveloperList().contains(user);
+        if (managerContains | stakeHolderContains | developerContains)
+            return new ResponseEntity<List<Task>>(taskService.getAllByStory(story), HttpStatus.OK);
+        return requestValidationService.getAccessDeniedResponseEntity();
     }
 }
