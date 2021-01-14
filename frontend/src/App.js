fcf3ec0f98e05899d511af8543a3b6208375a953
Dursun ERYILMAZ <dursun1147@gmail.com>
2021-01-14 20:45:46 +0300

    update: figured out a way to use layout
    react composition
    react function based components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Wrapper from "./components/Layout/Wrapper";
 function App() {
   return (
     <div className="App">
-      <Wrapper />
+      <Wrapper>Hello react compositions.</Wrapper>
     </div>
   );
 }
