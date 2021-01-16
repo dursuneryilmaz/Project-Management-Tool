7bc12e3f4a39f97629f8001a757ebfd8456e61be
Dursun ERYILMAZ <dursun1147@gmail.com>
2021-01-16 18:41:38 +0300

    update: divide html to small jsx components

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,47 +1,27 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from "react";
+import AlertBox from "./Header/AlertBox";
+import MessageBox from "./Header/MessageBox";
+import SearchBox from "./Header/SearchBox";
+import UserBox from "./Header/UserBox";
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
-          <div className="container">
-            <a className="navbar-brand" href="dashboard">
-              DuScrumTool
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#mobile-nav"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="mobile-nav">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="/dashboard">
-                    Dashboard
-                  </a>
-                </li>
-              </ul>
-
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link " href="register">
-                    Sign Up
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="login">
-                    Login
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+        {/* Header Nav Start */}
+        <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+          {/* Search Box */}
+          <SearchBox />
+          <ul className="navbar-nav ml-auto">
+            {/* alerts box */}
+            <AlertBox />
+            {/* message box */}
+            <MessageBox />
+            <div className="topbar-divider d-none d-sm-block"></div>
+            {/* User box */}
+            <UserBox />
+          </ul>
         </nav>
       </div>
     );
